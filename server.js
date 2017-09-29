07d5414f16473f41bcf7c9d0e387cfd423f91747
shabab477 <shababkarim93@gmail.com>
2017-09-30 00:15:19 +0600

    test22

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const config = require("./config") 
-const express = require("express")
+import config from "./config"
+import express from "express"
+
 const server = express()
 
 server.get("/", (req, res) => {
 	res.send("hello world")
 })
+
+console.log("object " + config);
+console.log("imported port " + config.port);
 
 console.log("port num is " + process.env.PORT);
 server.listen(config.port, () => {
