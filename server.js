ea97f0cb3871c01fcd64a2155820486df66d24b9
shabab477 <shababkarim93@gmail.com>
2017-09-29 23:17:20 +0600

    test1

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-import express from "express"
-
-const server = express()
-
-server.get("/", (req, res) => {
-	res.send("hello world")
-})
-
-
-server.listen(config.port, ()=>{
-	console.log("Listening from express")
-})
+console.log("hello world");
