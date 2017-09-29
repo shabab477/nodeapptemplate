7e298988426956a78ea0b1ae8d26a1a2cf95098b
shabab477 <shababkarim93@gmail.com>
2017-09-29 23:38:21 +0600

    test10

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-let express = require("express")
+const config = require("./config") 
+const express = require("express")
 const server = express()
 
 server.get("/", (req, res) => {
 	res.send("hello world")
+})
+
+server.listen(config.port, () => {
+	console.log("listening for events")
 })
